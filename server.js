f5712fef77a25de19d8abe8e2236238425bfe6f0
Jeffrey Sun <jrsun@mit.edu>
2017-05-02 14:37:03 -0700

    add config

--- server.js
+++ server.js
@@ -46,4 +46,7 @@ cluster((worker) => {
     app.listen(activePort, () => {
         console.log('worker ' + worker.id + ' is listening on port ' + activePort);
     });
+}, {
+    'respawn': true, // workers will restart on failure
+    'verbose': true, // logs what happens to console
 })
