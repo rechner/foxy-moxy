5f34b2af8818fc58e10fb9a821d307573abcc524
Jeffrey Sun <jrsun@mit.edu>
2017-06-12 18:16:26 -0700

    americanize fox colors

---
+++ js/constants/colors.js
@@ -0,0 +1,16 @@
+var head = {
+    brick: [0, 62, 53],
+    yellow: [48, 100, 64]
+}
+var bg = {
+    forest:     '#006375',
+    green:      '#63D6A3',
+    blue:       '#358EFF',
+    salmon:     '#FF9B7A',
+    coral:      '#F96854',
+}
+
+module.exports = {
+    head: head,
+    bg: bg
+};
