c5a8acc12db01ab75884229bac2e914cc62e7bb5
joshbuddy <josh@patreon.com>
2017-06-12 18:39:14 -0700

    Linting with "test"

--- js/constants/colors.js
+++ js/constants/colors.js
@@ -1,16 +1,16 @@
 const head = {
-    brick: [0, 62, 53],
-    yellow: [48, 100, 64]
+  brick: [0, 62, 53],
+  yellow: [48, 100, 64]
 }
 const bg = {
-    forest:     '#006375',
-    green:      '#63D6A3',
-    blue:       '#358EFF',
-    salmon:     '#FF9B7A',
-    coral:      '#F96854',
+  forest: '#006375',
+  green: '#63D6A3',
+  blue: '#358EFF',
+  salmon: '#FF9B7A',
+  coral: '#F96854'
 }
 
 module.exports = {
-    head: head,
-    bg: bg
-};
+  head: head,
+  bg: bg
+}
--- run.js
+++ run.js
@@ -1,13 +1,13 @@
-const cluster = require('express-cluster');
-const app = require('./server.js');
+const cluster = require('express-cluster')
+const app = require('./server.js')
 
-const activePort = process.env.PORT || 3000;
+const activePort = process.env.PORT || 3000
 
 cluster((worker) => {
-    app.listen(activePort, () => {
-        console.log('worker ' + worker.id + ' is listening on port ' + activePort);
-    });
+  app.listen(activePort, () => {
+    console.log('worker ' + worker.id + ' is listening on port ' + activePort)
+  })
 }, {
-    'respawn': true, // workers will restart on failure
-    'verbose': true, // logs what happens to console
-});
+  'respawn': true, // workers will restart on failure
+  'verbose': true // logs what happens to console
+})
